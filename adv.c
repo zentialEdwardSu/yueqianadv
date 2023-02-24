d290994b39331b028f15c4adc35eceb9bc7ff3c1
chiitt <1354283438@qq.com>
2023-02-24 21:18:18 +0800

    Update finish adv.c with base func

--- adv.c
+++ adv.c
@@ -1,8 +1,38 @@
-#include "LCDutils.h"// provide basic screen 
-#include "LCDjpeg.h"
+#include "include/LCDjpeg.h"
+#include "include/LCDutils.h"
 
+#include <stdlib.h>
+#include <unistd.h>
 
+void Black(Screen s);
 
-int main() {
+int main()
+{
+    Screen s = Screen_new("/dev/fb0",800, 480); //打开显示器
 
+    Black(s);
+
+    LCDjpeg* j1 = LCDjpeg_read("1.jpg");//显示图片
+    LCDjpeg_print_to_screen(j1 , s);
+    sleep(10);
+
+    system("mplayer  1.avi   -zoom  -x  800  -y  480");//播放视频
+    sleep(10);
+
+    LCDjpeg* j2 = LCDjpeg_read("2.jpg");//显示图片
+    LCDjpeg_print_to_screen(j2 , s);
+    sleep(10);
+
+    Black(s);
+    LCDjpeg_destory(j1);
+    LCDjpeg_destory(j2);
+
+    return 0;
+}
+
+void Black(Screen s)
+{
+    int c = mix_arbg(0,0,0,0);
+
+    write_region(&c,Screen_to_region(s),s);
 }
